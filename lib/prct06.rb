e01747521fb71755e7dbc775618d64f9b77d1b34
alu0100979602 <alu0100979602@ull.edu.es>
2018-11-08 16:24:28 +0000

    Método mostrar() correcto

--- lib/prct06.rb
+++ lib/prct06.rb
@@ -34,4 +34,22 @@ class Etiqueta
 		total = @valor_energ+@grasas+@grasas_s+@hidratos+@azucares+@proteinas+@sal+@grasas_m+@grasas_p+@poli_alc+@almidon+@fibra+@vitaminas+@minerales
 		(ref*100)/total
 	end
-end 
+
+	def mostrar
+	 
+		puts "Nombre etiqueta: #{@nombre}"		        
+		puts  "Valor energético: #{self.calcularkjg} kj\t / #{self.calcularkcg} kcal"
+		puts  "Grasas: #{@grasas} g\t #{self.calcularir(@grasas)}%"
+		puts  "Grasas saturadas: #{@grasas_s} g\t #{self.calcularir(@grasas_s)}%"
+		puts  "Grasas monoinsaturadas: #{@grasas_m} g\t #{self.calcularir(@grasas_m)}%"
+		puts  "Grasas poliinsaturadas: #{@grasas_p} g\t #{self.calcularir(@grasas_p)}%"
+		puts  "Hidratos: #{@hidratos} g\t #{self.calcularir(@hidratos)}%"
+		puts  "Azúcares: #{@azucares} g\t #{self.calcularir(@azucares)}%"
+		puts  "Polialcoholes: #{@poli_alc} g\t #{self.calcularir(@poli_alc)}%"
+		puts  "Almidon: #{@almidon} g\t #{self.calcularir(@almidon)}%"
+		puts  "Fibra: #{@fibra} g\t #{self.calcularir(@fibra)}%"
+		puts  "Proteínas: #{@proteinas} g\t #{self.calcularir(@proteinas)}%"
+		puts  "Vitaminas: #{@vitaminas} g\t #{self.calcularir(@vitaminas)}%"
+		puts  "Minerales: #{@minerales} g\t #{self.calcularir(@minerales)}%\n"
+	end
+end
