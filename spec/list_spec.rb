e789122f2a2e6786cb264f01a7e7e47daf4e2e92
alu0100979602 <alu0100979602@ull.edu.es>
2018-11-15 17:23:16 +0000

    Falta constructor de la lista

--- spec/list_spec.rb
+++ spec/list_spec.rb
@@ -4,6 +4,8 @@ RSpec.describe Etiqueta do
 		@et1 = Etiqueta.new("Platano",110,15,0,0,0,56,4,0,1,9,8,3,1.5,3)
 		@et2 = Etiqueta.new("Gofio",40,30,25,2.3,56,0,0,0,0,0,3,3,4.5,7)
 		@et3 = Etiqueta.new("Higo",80,34,67,0,1,3,2.1,5,6,4,2,1,7.8,0)
+
+		@l = List.new()
 	end
 
 
