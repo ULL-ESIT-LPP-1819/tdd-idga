97805ca83e889b0af9cacd2336db20c7a25948a1
alu0100979602 <alu0100979602@ull.edu.es>
2018-11-15 17:21:25 +0000

    Creación de los distintos alimentos

--- spec/list_spec.rb
+++ spec/list_spec.rb
@@ -1,6 +1,10 @@
 RSpec.describe Etiqueta do
 
-
+	before :each do
+		@et1 = Etiqueta.new("Platano",110,15,0,0,0,56,4,0,1,9,8,3,1.5,3)
+		@et2 = Etiqueta.new("Gofio",40,30,25,2.3,56,0,0,0,0,0,3,3,4.5,7)
+		@et3 = Etiqueta.new("Higo",80,34,67,0,1,3,2.1,5,6,4,2,1,7.8,0)
+	end
 
 
 end
