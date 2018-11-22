8da86372b9194f53de512433a0bd7a9a14a7e345
alu0100979602 <alu0100979602@ull.edu.es>
2018-11-22 18:53:10 +0000

    Prueba 1 fallida y superada

--- spec/person_spec.rb
+++ spec/person_spec.rb
@@ -1,6 +1,22 @@
-Rspec.describe Etiqueta do
+RSpec.describe Etiqueta do
 	
 	before :each do
-		@person1 = Paciente.new("Pepito", Data.new(175,70,15,0,100,110))	
+
+		# Ejemplos para la lista
+		@person1 = Patient.new("Pepito", Datos.new(175,70,15,1,100,110))
+		@person2 = Patient.new("Maria", Datos.new(164,60,19,0,98,100))
+		@person3 = Patient.new("Mario", Datos.new(168,50,24,1,90,91))
+		@person4 = Patient.new("Lucia", Datos.new(180,67,21,0,96,100))
+		@person5 = Patient.new("Manolo", Datos.new(176,70,16,1,90,80))
+
+		# Persona que no es paciente
+		@person6 = Person.new("Juan")
+
+		@l = List.new()	
+	end
+
+	it "Comprobar la clase del objeto" do
+		expect(@person1).to be_a_kind_of(Person)
+		expect(@person1).to be_a_kind_of(Patient)
 	end
 end
