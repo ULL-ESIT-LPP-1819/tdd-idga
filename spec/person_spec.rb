a9b2f741252c9a52203da439525928c150183c92
alu0100979602 <alu0100979602@ull.edu.es>
2018-12-06 15:16:44 +0000

    Pruebas Comparable Persona fallidas

--- spec/person_spec.rb
+++ spec/person_spec.rb
@@ -4,8 +4,8 @@ RSpec.describe Etiqueta do
 
 		# Ejemplos para la lista
 		@person1 = Patient.new("Pepito", Datos.new(175,70,15,1,100,110))
-		@person2 = Patient.new("Maria", Datos.new(164,60,19,0,98,100))
-		@person3 = Patient.new("Mario", Datos.new(168,50,24,1,90,91))
+		@person2 = Patient.new("Marco", Datos.new(168,60,19,1,98,100))
+		@person3 = Patient.new("Mario", Datos.new(168,60,19,1,98,100))
 		@person4 = Patient.new("Lucia", Datos.new(180,67,21,0,96,100))
 		@person5 = Patient.new("Manolo", Datos.new(178,80,16,1,90,80))
 
@@ -38,4 +38,30 @@ RSpec.describe Etiqueta do
 		expect(array[0].length).to eq(5)
 		expect(array[1].length).to eq(0)
 	end
+
+	### Pruebas comparación Persona ###
+	
+	it "[Persona] <" do
+		expect(@person1 < @person2).to be true
+      	end
+
+  	it "[Persona] <=" do
+    		expect(@person1 <= @person2).to be true
+    	end
+
+    	it "[Persona] ==" do
+      		expect(@person2 == @person3).to be false
+      	end
+
+      	it "[Persona] >" do
+        	expect(@person4 > @person2).to be false
+        end
+
+  	it "[Persona] >=" do
+    		expect(@person1 >= @person4).to be false
+    	end
+
+    	it "[Persona] Between" do
+      		expect(@person3.between?(@person1, @person4)).to be false
+        end	
 end
